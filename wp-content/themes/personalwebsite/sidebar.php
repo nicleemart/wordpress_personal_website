a0d5f7b1797e81aa1f73cb5124984278bf2dfa0e
Nikki <nicleemart@gmail.com>
2016-08-09 14:35:41 -0500

    added social media images

--- wp-content/themes/personalwebsite/sidebar.php
+++ wp-content/themes/personalwebsite/sidebar.php
@@ -3,9 +3,9 @@
 	<div class="name">nikki martin</div>
 
 	<div class="socialMedia">
-		<a href="https://www.facebook.com/nikki.lee.martin" target="_blank"><img src="<?php echo "$directory_prefix" . "images/fb.png" ?>" alt="Facebook" class="socialMedia__image"></a>
-		<a href="https://github.com/nicleemart" target="_blank"><img src="<?php echo "$directory_prefix" . "images/github.png" ?>" alt="GitHub" class="socialMedia__image"></a>
-		<a href="https://www.linkedin.com/in/nikki-martin-7b9645a2" target="_blank"><img src="<?php echo "$directory_prefix" . "images/linkedin.png" ?>" alt="LinkedIn" class="socialMedia__image"></a>
+		<a href="https://www.facebook.com/nikki.lee.martin" target="_blank"><img src="http://localhost:8888/wordpress_personal_website/wp-content/uploads/2016/08/fb.png" alt="Facebook" class="socialMedia__image"></a>
+		<a href="https://github.com/nicleemart" target="_blank"><img src="http://localhost:8888/wordpress_personal_website/wp-content/uploads/2016/08/github.png" alt="GitHub" class="socialMedia__image"></a>
+		<a href="https://www.linkedin.com/in/nikki-martin-7b9645a2" target="_blank"><img src="http://localhost:8888/wordpress_personal_website/wp-content/uploads/2016/08/linkedin.png" alt="LinkedIn" class="socialMedia__image"></a>
 	</div>
 
 </div>
